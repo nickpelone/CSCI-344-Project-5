26446f55476d363ff4d917e5e0a0db8a0ca2ef8b
Nick Pelone <npelone@unca.edu>
2013-04-30 15:30:58 -0400

    Adds basic mongoose model for tasks in the todo list

--- node-dev-bootstrap/app/server.js
+++ node-dev-bootstrap/app/server.js
@@ -1,4 +1,23 @@
-var http = require('http');
+var http = require('http'),
+    mongo = require('mongodb'),
+    express = require('express'),
+    app = express(),
+    mongoose = require('mongoose');
+    mongoose.connect('mongodb://localhost/test');
+
+var TaskModel = mongoose.model('Task', {number: Number, description: String, categories: [String]});
+
+var Task1 = new TaskModel;
+Task1.number = 1;
+Task1.description = "Something I gotta do";
+Task1.categories = ["default", "derp"];
+Task1.save(function (err,res){
+    if (err) throw err;
+    console.log("Task1.save returned: " + res);
+});
+
+    
+    
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
